8d17721fe298740171139140abd2e6d9b24b30eb
Kyrela <mathis.vinay@gmail.com>
2024-06-24 12:33:59 +0200

    Corrected style code

--- discord_markdown_ast_parser/lexer.py
+++ discord_markdown_ast_parser/lexer.py
@@ -1,5 +1,5 @@
 import re
-from dataclasses import dataclass, InitVar, field
+from dataclasses import dataclass, field
 from enum import Enum
 from typing import Optional, List, Generator, Dict
 import itertools
@@ -8,10 +8,10 @@ import itertools
 class Lexing:
     def __init__(self, pattern: Optional[str] = None, flags: re.RegexFlag = re.NOFLAG):
         self.regex = re.compile(pattern, flags=flags) if pattern else None
-        
+
     def __call__(self, text: str):
         return self.regex and self.regex.match(text)
-    
+
     def __repr__(self):
         return f"{self.__class__.__name__}({self.regex and self.regex.pattern!r})"
 
@@ -22,16 +22,16 @@ URL_REGEX = r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a
 class LexingRule(Lexing, Enum):
     USER_MENTION = r"<@!?(\d{15,20})>"
     ROLE_MENTION = r"<@&(\d{15,20})>"
-    SLASH_COMMAND_MENTION  = r"</([a-zA-Z0-9_ ]{2,}):(\d{15,20})>"
+    SLASH_COMMAND_MENTION = r"</([a-zA-Z0-9_ ]{2,}):(\d{15,20})>"
     CHANNEL_MENTION = r"<#(\d{15,20})>"
     TIMESTAMP = r"<t:(-?\d+)(?::([tTdDfFR]))?>"
     EMOJI_CUSTOM = r"<:([a-zA-Z0-9_]{2,}):(\d{15,20})>"
     EMOJI_CUSTOM_ANIMATED = r"<a:([a-zA-Z0-9_]{2,}):(\d{15,20})>"
     EMOJI_UNICODE = r"(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])"
     EMOJI_UNICODE_ENCODED = r":([a-zA-Z0-9_]+):"
-    URL_WITHOUT_PREVIEW_EMBEDDED = f"\[([^\]]+)\]\(<({URL_REGEX})>\)"
-    URL_WITH_PREVIEW_EMBEDDED =    f"\[([^\]]+)\]\(({URL_REGEX})\)"
-    URL_WITHOUT_PREVIEW = f"<{URL_REGEX}>"
+    URL_WITHOUT_PREVIEW_EMBEDDED = fr"\[([^\]]+)\]\(<({URL_REGEX})>\)"
+    URL_WITH_PREVIEW_EMBEDDED = fr"\[([^\]]+)\]\(({URL_REGEX})\)"
+    URL_WITHOUT_PREVIEW = fr"<{URL_REGEX}>"
     URL_WITH_PREVIEW = URL_REGEX
     QUOTE_LINE_PREFIX = r"(>>)?> "
     TILDE = r"~"
@@ -49,10 +49,9 @@ class Token:
     value: str = ""
     lexing_rule: Lexing = LexingRule.TEXT_INLINE
     groups: List[str] = field(default_factory=list)
-    
+
     def __contains__(self, rule: Lexing):
         return self.lexing_rule == rule
-
 
 
 def lex(input_text: str, custom: Optional[Dict[str, List[Lexing]]] = None) -> Generator[Token, None, None]:
@@ -67,7 +66,7 @@ def lex(input_text: str, custom: Optional[Dict[str, List[Lexing]]] = None) -> Ge
     """
     seen_simple_text = ""
     custom = custom or {}
-    
+
     while input_text:
         for rule in itertools.chain(*custom.values(), LexingRule):
             match = rule(input_text)
@@ -80,7 +79,7 @@ def lex(input_text: str, custom: Optional[Dict[str, List[Lexing]]] = None) -> Ge
             continue  # don't yield a token in this run
 
         # cut off matched part
-        input_text = input_text[len(match[0]) :]
+        input_text = input_text[len(match[0]):]
 
         # yield inline text if we have some left
         if len(seen_simple_text) > 0:
